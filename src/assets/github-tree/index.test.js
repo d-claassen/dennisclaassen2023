a8a3f7c2cf5d687fe9e33e13b48b6ff0b6e833bf
Dennis Claassen <declaassen@gmail.com>
2023-06-29 00:20:01 +0200

    Clean spaces

--- src/assets/github-tree/index.test.js
+++ src/assets/github-tree/index.test.js
@@ -1,5 +1,5 @@
-describe( 'the test', function () {
-	test( 'what it does', function () {
+describe( 'the test', function() {
+	test( 'what it does', function() {
 		expect( true ).toBe( true );
 	} );
 } );
