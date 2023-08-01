6be0706f446cc83683ad1e6e7bd012f8ea6b0535
Dennis Claassen <declaassen@gmail.com>
2023-08-01 22:16:57 +0200

    test the test

--- tests/FirstTest.php
+++ tests/FirstTest.php
@@ -4,7 +4,18 @@ namespace DC23\Tests;
 
 class FirstTest extends \PHPUnit\Framework\TestCase {
 
+	/**
+	 * Covers nothing.
+	 *
+	 * @coversNothing
+	 */
 	public function testTheTest(): void {
 		$this->assertEquals( 1, '1' );
+
+		$this->assertSame(
+			$this->testTheTest( ... ),
+			$this->testTheTest( ... ),
+			'same method'
+		);
 	}
 }
