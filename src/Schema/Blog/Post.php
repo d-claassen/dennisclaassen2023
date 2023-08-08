ca43e7eb7b3f7271fbaaaf84e72672b2077ba367
Dennis Claassen <declaassen@gmail.com>
2023-08-08 07:37:23 +0200

    Update Post.php

--- src/Schema/Blog/Post.php
+++ src/Schema/Blog/Post.php
@@ -25,7 +25,7 @@ final class Post {
 			return $pieces;
 		}
 
-		$canonical = YoastSEO()->meta->for_current_page()->canonical;
+		$canonical = \YoastSEO()->meta->for_current_page()->canonical;
 
 		$post = \get_post();
 		assert( $post instanceof \WP_Post );
