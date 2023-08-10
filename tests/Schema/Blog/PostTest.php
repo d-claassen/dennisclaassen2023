edaab091b14da1e0aed88bb4e5ebde432e9d1d81
Dennis Claassen <declaassen@gmail.com>
2023-08-10 23:09:08 +0200

    Update PostTest.php

--- tests/Schema/Blog/PostTest.php
+++ tests/Schema/Blog/PostTest.php
@@ -473,9 +473,10 @@ class PostTest extends \PHPUnit\Framework\TestCase {
 
 		$this->options_helper->expects('get')->with('company_or_person', false)->andReturns('person');
 		$this->options_helper->expects('get')->with('company_or_person_user_id', false)->andReturns(1);
-		\Brain\Monkey\Functions\expect('get_user_by')
-			->with('id', 1)
-			->andReturn( \Mockery::mock( \WP_User::class ) );
+		$user = \Mockery::mock( \WP_User::class );
+		$user->user_login = 'info@example.com';
+		\Brain\Monkey\Functions\expect('get_user_by')->with('id', 1)->andReturn( $user );
+		\Brain\Monkey\Functions\expect('get_userdata')->with('id', 1)->andReturn( $user );
 		
 		/*
 		$this->indexable_repository
