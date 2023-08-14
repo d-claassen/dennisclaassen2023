b99afa307f2711cc447c1c05bb4ea75d8739b12d
Dennis Claassen <declaassen@gmail.com>
2023-08-14 22:27:25 +0200

    Create FirstTest.php

---
+++ tests/FirstTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DC23\Tests;
+
+class FirstTest extends \PHPUnit\Framework\TestCase {
+
+	/**
+	 * Covers nothing.
+	 *
+	 * @coversNothing
+	 */
+	public function testTheTest(): void {
+		$this->assertEquals( 1, '1' );
+	}
+}
