1362ba3cc257930a250da07daf3ae3d99cee2423
Dennis Claassen <declaassen@gmail.com>
2023-08-18 23:41:54 +0200

    Update PostTest.php

--- tests/Schema/Blog/PostTest.php
+++ tests/Schema/Blog/PostTest.php
@@ -442,9 +442,6 @@ class PostTest extends TestCase {
 		$context = $this->getContext();
 		$context->indexable->schema_article_type = 'BlogPosting';
 		$context->canonical = 'https://example.com/';
-
-		$this->options_helper->expects('get')->with('company_or_person', false)->andReturns('person');
-		$this->options_helper->expects('get')->with('company_or_person_user_id', false)->andReturns(1);
 		
 		$user = \Mockery::mock( \WP_User::class );
 		$user->user_login = 'info@example.com';
