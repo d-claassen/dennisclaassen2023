d12fc1f94ad1d0b179c5e06f6cee5b77e0448746
Dennis Claassen <declaassen@gmail.com>
2023-08-29 23:56:35 +0200

    Create CategoryTest.php

---
+++ tests/Schema/Blog/CategoryTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use DC23\Schema\Blog\Category;
+use PHPUnit\Framework\TestCase;
+
+class CategoryTest extends TestCase {
+
+	public function setUp(): void {
+		parent::setUp();
+		\Brain\Monkey\setUp();
+	}
+
+	public function tearDown(): void {
+		\Brain\Monkey\tearDown();
+		parent::tearDown();
+	}
+	public function testRegistersFilters(): void {
+		$category = new Category();
+		$category->register();
+
+		self::assertSame(11, has_filter( 'wpseo_schema_webpage', [ $category, 'make_blog_main_entity' ] ) );
+		self::assertSame(11, has_filter( 'wpseo_schema_graph_pieces', [ $category, 'add_blog_to_schema' ] ) );
+	}
+}
