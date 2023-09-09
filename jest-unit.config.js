73394d71e02453ffdd9dbaea8c6debdab35e7151
Dennis Claassen <declaassen@gmail.com>
2023-09-09 15:52:01 +0200

    Update jest-unit.config.js

--- jest-unit.config.js
+++ jest-unit.config.js
@@ -3,7 +3,7 @@ const wpJestConfig = require( '@wordpress/scripts/config/jest-unit.config.js' );
 const config = {
 	...wpJestConfig,
 	collectCoverageFrom: [ 'src/**' ],
-	coveragePathIgnorePatterns: [ '(/specs/)', '(\.spec\.[jt]s?$)' ],
+	coveragePathIgnorePatterns: [ '(/specs/)', '(\\.spec\\.[jt]s?$)' ],
 };
 
 module.exports = config;
