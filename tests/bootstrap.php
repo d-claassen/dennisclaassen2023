7454fc1a7fc6c7f8f7a832f538c2f538dc5c2708
Dennis Claassen <declaassen@gmail.com>
2023-09-12 21:40:26 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,3 +2,6 @@
 
 // require_once __DIR__ . '/../vendor/autoload.php';
 // require_once __DIR__ . '/../vendor/yoast/wordpress-seo/src/functions.php';
+
+register_theme_directory( __DIR__ .'/../../' );
+switch_theme( 'dennisclaassen2023' );
