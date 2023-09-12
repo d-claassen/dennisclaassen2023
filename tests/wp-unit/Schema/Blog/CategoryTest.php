c05cd219a6d7b1b89837ac7dccefc3ad331407da
Dennis Claassen <declaassen@gmail.com>
2023-09-12 20:52:24 +0200

    Update CategoryTest.php

--- tests/wp-unit/Schema/Blog/CategoryTest.php
+++ tests/wp-unit/Schema/Blog/CategoryTest.php
@@ -2,6 +2,10 @@
 
 class CategoryTest extends \WP_UnitTestCase {
 
+	// override wordpress function thats incompatible
+	// with phpunit 10.
+	public function expectDeprecated(){}
+	
 	public function testIt(): void {
 		$this->assertTrue( false );
 	}
