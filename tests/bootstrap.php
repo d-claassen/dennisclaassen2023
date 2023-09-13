4e73817673e7593f3f1b81bbebe4cfc50b534415
Dennis Claassen <declaassen@gmail.com>
2023-09-13 23:38:28 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,15 @@
 <?php declare(strict_types=1);
 
+use function \Yoast\WPTestUtils\WPIntegration\get_path_to_wp_test_dir;
+
 // require_once __DIR__ . '/../vendor/autoload.php';
 // require_once __DIR__ . '/../vendor/yoast/wordpress-seo/src/functions.php';
+/* *****[ Wire in the integration ]***** */
+
+$_tests_dir = get_path_to_wp_test_dir();
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . 'includes/functions.php';
 
 function load_plugins() {
 	require_once __DIR__ . '/../vendor/yoast/wordpress-seo/wp-seo.php';
