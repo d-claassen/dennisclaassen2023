0968d6a2a3efc1c4d9da11048eb3fcc9c3755304
Dennis Claassen <declaassen@gmail.com>
2023-09-13 23:18:34 +0200

    Load Yoast SEO in tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,12 @@
 // require_once __DIR__ . '/../vendor/autoload.php';
 // require_once __DIR__ . '/../vendor/yoast/wordpress-seo/src/functions.php';
 
+function load_plugins() {
+	require_once __DIR__ . '/../vendor/yoast/wordpress-seo/wp-seo.php';
+}
+
+// Add plugin to active mu-plugins - to make sure it gets loaded.
+tests_add_filter( 'muplugins_loaded', load_plugins(...) );
+
 register_theme_directory( __DIR__ .'/../../' );
 switch_theme( 'dennisclaassen2023' );
