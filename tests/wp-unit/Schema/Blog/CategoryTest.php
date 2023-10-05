1edd18379bbc6604cedcc39abbcae40a29db2630
dennis <dennis@yoast.com>
2023-10-05 08:53:25 +0200

    Create custom wp unit bootstrap

--- tests/wp-unit/Schema/Blog/CategoryTest.php
+++ tests/wp-unit/Schema/Blog/CategoryTest.php
@@ -5,25 +5,43 @@ class CategoryTest extends \WP_UnitTestCase {
 	// override wordpress function thats incompatible
 	// with phpunit 10.
 	public function expectDeprecated(){}
-	
+
 	public function testIt(): void {
-		$post = self::factory()->post->create(
-			array( 
+		$post_id = self::factory()->post->create(
+			array(
 				'post_content' => 'Hello world!',
 			)
 		);
 
-		$post_filled = new \WP_Query( 
-			array(
-				'p' => $post,
-			)
-		);
-		while ( $post_filled->have_posts() ) {
-			$post_filled->the_post();
+		$this->go_to( \get_permalink( $post_id ) );
 
-			do_action( 'wpseo_head' );
-		}
-		
+		$schema_output = $this->get_schema_output( true );
+
+		$this->assertJson( $schema_output );
+
 		$this->assertSame( 'en-US', get_bloginfo( 'language' ) );
+	}
+
+	private function get_schema_output( bool $debug_wpseo_head = false ): string {
+
+		ob_start();
+		do_action( 'wpseo_head' );
+		$wpseo_head = ob_get_contents();
+		ob_end_clean();
+
+		if ( $debug_wpseo_head ) {
+			print $wpseo_head;
+		}
+
+		$dom = new \DOMDocument();
+		$dom->loadHTML( $wpseo_head );
+		$scripts = $dom->getElementsByTagName('script');
+		foreach( $scripts as $script ) {
+			if( $script instanceof \DOMElement && $script->getAttribute('type') === 'application/ld+json') {
+				return $script->textContent;
+			}
+		}
+
+		throw new \LengthException('No schema script was found in the wpseo_head output.' );
 	}
 }
