73cbbc0849f4dcdd47763dbed8d342e91d7696be
Dennis Claassen <declaassen@gmail.com>
2024-01-22 21:28:20 +0100

    PHPCS update: Prepare generic Piece

--- src/Schema/Piece.php
+++ src/Schema/Piece.php
@@ -8,9 +8,17 @@ use Yoast\WP\SEO\Generators\Schema\Abstract_Schema_Piece;
 
 /**
  * Generic Schema.org piece to wrap any kind of data.
+ *
+ * @template T of array<string, mixed>
  */
 class Piece extends Abstract_Schema_Piece {
 
+	/**
+	 * Constructor.
+	 *
+	 * @param T $output The Schema.org output.
+	 * @param string|null $identifier The piece identifier to detect potential duplicates.
+	 */
 	public function __construct(
 		private array $output,
 		public $identifier = null
@@ -20,6 +28,11 @@ class Piece extends Abstract_Schema_Piece {
 		return true;
 	}
 
+	/**
+	 * Return the Schema.org output.
+	 *
+	 * @return T The schema output
+	 */
 	public function generate(): array {
 		return $this->output;
 	}
