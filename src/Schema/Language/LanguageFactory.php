d1922bf61df750bfd30e7868f2c27eed8db497e7
Dennis Claassen <declaassen@gmail.com>
2024-01-23 00:19:46 +0100

    PHPCS: Update LanguageFactory.php

--- src/Schema/Language/LanguageFactory.php
+++ src/Schema/Language/LanguageFactory.php
@@ -5,7 +5,7 @@ namespace DC23\Schema\Language;
 
 final class LanguageFactory {
 
-	public function create_language( string $locale ) {
+	public function create_language( string $locale ): Language {
 		switch ( $locale ) {
 			case 'en-US':
 				return new Language( 'en-US', 'English (American)', 'https://en.wikipedia.org/wiki/American_English' );
