932ba5cc463256e3e7ce52f7af8ab4383fa2b10b
Dennis Claassen <declaassen@gmail.com>
2024-02-26 23:28:55 +0100

    Debug

--- tests/wp-unit/Schema/Language/SiteLanguageTest.php
+++ tests/wp-unit/Schema/Language/SiteLanguageTest.php
@@ -42,7 +42,8 @@ final class SiteLanguageTest extends \WP_UnitTestCase {
 		\update_option( 'show_on_front', 'page' );
 		\update_option( 'page_on_front', $post_id );
 
-		$this->go_to( get_home_url() );
+		\var_dump( $home_url = get_home_url() );
+		$this->go_to( $home_url );
 
         $schema_output = $this->get_schema_output();
 		$this->assertJson( $schema_output );
