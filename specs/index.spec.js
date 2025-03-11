a3a1c5c0407ee9abe695f252bbeb5319bb3aecbc
Dennis Claassen <declaassen@gmail.com>
2025-03-11 06:24:08 +0100

    Update index.spec.js

--- specs/index.spec.js
+++ specs/index.spec.js
@@ -1,4 +1,6 @@
-describe( 'the test', function () {
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'the test', function () {
 	test( 'what it does', function () {
 		expect( true ).toBe( true );
 	} );
