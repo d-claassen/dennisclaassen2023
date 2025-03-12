7b237050ebc07c7ce438111bdcbfae31e4619c82
Dennis Claassen <declaassen@gmail.com>
2025-03-12 01:20:36 +0100

    Enable e2e tests (#322)
    * Switch to Playwright 
    
    * Run e2e tests against fully functioning fresh WP env
    
    * fix: use playwright custom config to change meaning of relative path
    
    * moved spec files to dedicated folder per convention

---
+++ specs/index.spec.js
@@ -0,0 +1,7 @@
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'the test', function () {
+	test( 'what it does', function () {
+		expect( true ).toBe( true );
+	} );
+} );
