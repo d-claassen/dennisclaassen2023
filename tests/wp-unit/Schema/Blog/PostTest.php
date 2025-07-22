b7a71e5608f86bee8a0801b57826d48f89ca3084
Dennis Claassen <declaassen@gmail.com>
2025-07-23 00:19:59 +0200

    Remove a PostTest

--- tests/wp-unit/Schema/Blog/PostTest.php
+++ tests/wp-unit/Schema/Blog/PostTest.php
@@ -153,45 +153,6 @@ class PostTest extends \WP_UnitTestCase {
 		$this->assertSame([ 'Article', 'BlogPosting' ], $article_piece['@type'],'Article graph piece should be BlogPosting');
 	}
 
-	public function test_should_add_time_required_to_webpage(): void {
-		$post_id = self::factory()->post->create(
-			array(
-				'title'        => 'WebPage with estimated reading time',
-				'post_content' => <<<'EOL'
-				Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live 
-				the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large 
-				language ocean. A small river named Duden flows by their place and supplies it with the necessary
-				regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
-				Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic
-				life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for
-				the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands
-				of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t
-				listen. She packed her seven versalia, put her initial into the belt and made herself on the
-				way. When she reached the first hills of the Italic Mountains, she had a last view back on
-				the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline
-				of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she
-				continued her way. On her way she met a copy. The copy warned the Little Blind Text, that
-				where it came from it would have been rewritten a thousand times and everything that was left
-				from its origin would be the word "and".
-				EOL,
-			)
-		);
-
-		// Update object to persist meta value to indexable.
-		self::factory()->post->update_object( $post_id, [] );
-
-		$this->go_to( \get_permalink( $post_id ) );
-
-		$schema_output = $this->get_schema_output();
-
-		$this->assertJson( $schema_output );
-
-		$schema_data = \json_decode( $schema_output, JSON_OBJECT_AS_ARRAY );
-
-		$this->assertSame( 'WebPage', $schema_data['@graph'][1]['@type'], '2nd piece should be WebPage' );
-		$this->assertSame( 'PT1M', $schema_data['@graph'][1]['timeRequired'], 'timeRequired should be filled for WebPage' );
-	}
-
 	private function get_schema_output( bool $debug_wpseo_head = false ): string {
 
 		ob_start();
